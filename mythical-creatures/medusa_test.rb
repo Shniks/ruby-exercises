af7d63ac5385430dcb8ce6b488589a3a5b189a08
Josh Thompson <thompsonjoshd@gmail.com>
2018-01-19 10:07:54 -0700

    answers to medusa_test.rb

--- mythical-creatures/medusa_test.rb
+++ mythical-creatures/medusa_test.rb
@@ -11,13 +11,11 @@ class MedusaTest < Minitest::Test
   end
 
   def test_when_first_created_she_has_no_statues
-
     medusa = Medusa.new("Cassiopeia")
     assert medusa.statues.empty?
   end
 
   def test_when_staring_at_a_person_she_gains_a_statue
-
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
 
@@ -27,10 +25,8 @@ class MedusaTest < Minitest::Test
   end
 
   def test_when_staring_at_a_person_that_person_turns_to_stone
-
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
-
     refute victim.stoned?
     medusa.stare(victim)
     assert victim.stoned?
--- mythical-creatures/person.rb
+++ mythical-creatures/person.rb
@@ -1,17 +1,18 @@
+require 'pry'
 require_relative 'medusa'
 
 class Person
 
   attr_reader :name
+  attr_accessor :stoned
 
   def initialize(name)
     @name = name
+    @stoned = false
   end
 
   def stoned?
-    require 'pry'; binding.pry 
-    return false if @statues.count == 0
-    true
+    stoned
   end
 
 end
